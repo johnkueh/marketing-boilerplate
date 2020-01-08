c856ff9e696eb347afa2fd909e41fa657a94ac4d
John Kueh <yoongfook@gmail.com>
2020-01-09 10:54:46 +1100

    Added list of posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,44 @@
 import { Link } from "@reach/router";
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Blog: React.FC = () => (
+interface Props {
+  data: any;
+}
+
+const Blog: React.FC<Props> = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
+    {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+      <>
+        <h2>{frontmatter.title}</h2>
+        <p>{frontmatter.date}</p>
+        <p>{excerpt}</p>
+      </>
+    ))}
     <Link to="/">Back to home</Link>
   </Layout>
 );
+
+export const query = graphql`
+  query ALL_POSTS {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
